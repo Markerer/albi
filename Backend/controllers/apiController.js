008e89c996e2b8b1962fd5ef36cec01f973df397
Szövő Roland <szovoroli@sch.bme.hu>
2018-11-27 17:14:58 +0100

    => deleted

--- Backend/controllers/apiController.js
+++ Backend/controllers/apiController.js
@@ -1,9 +1,6 @@
 var Users = require('../models/userModel');
 var Flats = require('../models/flatModel');
-var Images = require('../models/image');
 var bodyParser = require('body-parser');
-const sharp = require('sharp');
-const fs = require('fs');
 const checkAuth = require('../middleware/check-auth');
 const jwt = require("jsonwebtoken");
 
@@ -11,7 +8,7 @@ module.exports = function (app) {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-    
+
 
 
 
@@ -83,170 +80,36 @@ module.exports = function (app) {
         })
 
         Users.findOne({ username: user.username }, function (err, tempUser) {
-            if (err){
+            if (err) {
                 return res.status(401).json({
                     message: "NOT OK"
-                  });
+                });
             }
             if (tempUser && user.password === tempUser.password) {
-            
+
                 const token = jwt.sign(
                     {
-                      email: tempUser.email,
-                      userId: tempUser._id
+                        email: tempUser.email,
+                        userId: tempUser._id
                     },
                     "secret",
                     {
                         expiresIn: "1h"
                     }
                 );
-                   return res.status(200).json({
+                return res.status(200).json({
                     message: "OK",
                     token: token
-                  });
-                
+                });
+
             }
             res.status(401).json({
                 message: "NOT OK"
-              });
+            });
         });
 
 
     })
-
-    //BEGIN Upload Image
-    const multer = require('multer')
-    const path = require('path');
-
-    const storage = multer.diskStorage({
-        destination: './public/uploads/',
-        filename: function (req, file, cb) {
-            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-        }
-    });
-
-    // Init upload
-    const upload = multer({
-        storage: storage,
-        limits: { fileSize: 1000000 },
-
-        fileFilter: function (req, file, cb) {
-
-            checkFileType(file, cb);
-        }
-    }).single('image');
-
-    //Check File Type
-    function checkFileType(file, cb) {
-        // Allowed ext
-        const filetypes = /jpeg|jpg|png/;
-        // Check ext
-        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-        // Check mime
-        const mimetype = filetypes.test(file.mimetype);
-
-        if (mimetype && extname) {
-            return cb(null, true);
-        } else {
-            cb('Error: Images Only!');
-        }
-    }
-
-    //Upload image
-    app.post('/flat/upload/:id', checkAuth, function (req, res) {
-        var name;
-        upload(req, res, (err) => {
-            name = req.file.filename;
-            if (err) {
-                res.send('err');
-            } else {
-                if (req.file == undefined) {
-                    res.send('Error: No File Selected!')
-                } else {
-
-
-                    var newImage = Images({
-                        flatID: req.params.id,
-                        fieldname: req.file.fieldname,
-                        originalname: req.file.originalname,
-                        encoding: req.file.encoding,
-                        mimetype: req.file.mimetype,
-                        destination: req.file.destination,
-                        filename: req.file.filename,
-                        path: req.file.path,
-                        size: req.file.size
-                    });
-
-                    Images.create(newImage, function (err, results) {
-                        if (err) throw err;
-                        res.send(results);
-                    });
-
-                }
-                console.log(name);
-                sharp('./public/uploads/' + name).resize({ width: 600 }).toBuffer(function (err, buffer) {
-                    // 100 pixels wide, auto-scaled height
-                    fs.writeFile('./public/uploads/' + name, buffer, function (e) {
-        
-                    });
-                }); 
-            }
-        });
-        
-    });
-
-    //END of upload Image
-
-    //Load Image
-    app.get('/flat/images/:flatId', function (req, res) {
-        Images.find({ flatID: req.params.flatId }, function (err, images) {
-            if (err) throw err;
-            res.send(images);
-            //kép átméretezés
-            /*images.forEach(image => {
-                sharp('./public/uploads/' + image.filename).resize({ width: 600 }).toBuffer(function (err, buffer) {
-                    // 100 pixels wide, auto-scaled height
-                    fs.writeFile('./public/uploads/' + image.filename, buffer, function (e) {
-
-                    });
-                });
-
-            });*/
-
-        }).select('_id filename');
-    });
-
-    app.get('/image/:imageID', function (req, res) {
-        Images.findById({ _id: req.params.imageID }, function (err, image) {
-            if (err) {
-                res.send("There is no image with this ID");
-            }
-
-            /*sharp('./public/uploads/' + image.filename).resize({ width: 600 }).toBuffer(function (err, buffer) {
-                // 100 pixels wide, auto-scaled height
-                fs.writeFile('./public/uploads/' + image.filename, buffer, function (e) {
-
-                });
-            });*/
-            res.send(image);
-        }).select('filename');
-
-    });
-
-
-    //delete image
-
-    app.delete('/image/:imageID', checkAuth, function (req, res) {
-
-        Images.findByIdAndRemove(req.params.imageID, function (err) {
-            if (err) {
-                res.send('Unsuccessful');
-            }
-
-            res.send('Success');
-        })
-
-    });
 
     app.delete('/flat/:flatID', checkAuth, function (req, res) {
 
@@ -444,5 +307,5 @@ module.exports = function (app) {
 
     });
 
-   
+
 }
