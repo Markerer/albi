936d1d8b4d4220a17eb9fedf5a4b561ca0b661c6
Szövő Roland <szovoroli@sch.bme.hu>
2018-11-27 15:22:01 +0100

    refactor http header

--- Backend/app.js
+++ Backend/app.js
@@ -15,19 +15,13 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'https://localhost:4200');
-
+    res.header("Access-Control-Allow-Origin", "*");
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-   
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,responseType');
-
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-
-    res.header("Access-Control-Allow-Origin", "*");
      // Request headers you wish to allow
     res.header(
         "Access-Control-Allow-Headers",
